2c80b3cf8564fa5473efc729d9ec5b75d53a6b5e
GuilhermeHVSantos <guilhermehvs@hotmail.com>
2022-05-09 17:07:38 -0300

    returning the server process

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.VITE_API_URL || 3333, ()=>{
+app.listen(process.env.API_URL || 3333, ()=>{
     console.log("HTTP server running!");
 });
